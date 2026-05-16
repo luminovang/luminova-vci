83a9cddeca36f768b0acbe6d776bc59a1299acee
Luminova <dev@luminova.ng>
2026-05-16 21:26:27 +0800

    New update and changes

--- luminova.sh
+++ luminova.sh
@@ -25,7 +25,7 @@ if ! (return 0 2>/dev/null); then
     set -euo pipefail
 fi
 
-readonly VERSION="1.1.0"
+readonly VERSION="1.2.0"
 readonly PRODUCTION=1
 readonly SELF_REPO_URL="https://github.com/luminovang/luminova-vci.git"
 readonly PACKAGE_REPO_URL="https://github.com/luminovang/framework.git"
